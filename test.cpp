303812236c45973086356d054af2c7438208719c
howhow <loveisbelief@qq.com>
2017-02-07 00:56:53 -0600

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,10 @@
+//void print_GB2312();
+	//print_GB2312();
+#include   <iostream>
+#include   <string>
+	using   namespace   std;
+	string getLetter(string strText);
+	int   main(int   argc, char*   argv[])
+	{
+		cout << getLetter("中华人民共和国") << endl;
+	}
